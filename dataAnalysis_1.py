797b688d00c888d15c0a7e8454238f27d0a3ae76
Raj M Shukla <rshukla@0562118xb1nx1l.rd.unr.edu>
2018-08-21 17:45:45 -0700

    results added

--- dataAnalysis_1.py
+++ dataAnalysis_1.py
@@ -24,41 +24,38 @@ for i, val in enumerate(flow):
 
 flowArray = np.asarray(flowArray)
 
-point = 120
-day = 4
+point = 65
 flowAtPoint = flowArray[:, :, point:point+1]
 
-print np.shape(flowAtPoint)
+array = flowArray[:, 100:150:, 5]
 
 diff  = [[] for i in range(0, 12)]
-mean  = [[] for i in range(0, 7)]
 attack = [[] for i in range(0, 12)]
 attack_type_list = [[] for i in range(0, 12)]
 attack_points = [[] for i in range(0, 12)]
 attack_type = [[] for i in range(0, 12)]
+meanFlow = []
 
-for i in range (0, 7):
-    for j in range (0, 288):
-        mean[i].append(np.mean(flowAtPoint[i:28:7, j, :])) 
+for i in range(0, 7):
+    print(np.mean(flowAtPoint[i:28:7, :, : ], axis=0))
+    meanFlow.append(np.squeeze(np.mean(flowAtPoint[i:28:7, :, : ], axis=0)))
+
          
-print (np.shape(mean))
-
-for i in range (0, len(mean)):
-    plt.plot(mean[i])
-    plt.show()
-
 for i in range(0, 288 - len(diff)):
     for j in range(0, len(diff)):
-        diff[j].append( np.absolute(np.mean(flowAtPoint[:, i:i+1, :]) - np.mean(flowAtPoint[:, i+j+1:i+j+1 + j+1, :])) )
-    
+        diff[j].append( np.absolute(np.mean(flowAtPoint[:, i:i+j+1, :]) - np.mean(flowAtPoint[:, i+j+1:i+j+1 + j+1, :])) )
+
+     
 threshhold = [max(i) for i in diff]
 
 def Analysis(start):
 
     end = start + start/100
-    flowWithAttack = flowAtPoint[day, 0:288, :] 
-    flowWithAttack[150:200:2] = flowWithAttack[150:200:2] + 2*random.uniform(start, end) 
-    flowWithAttack[151:200:2] = flowWithAttack[151:200:2] + 2*random.uniform(start, end)       
+    flowWithAttack = flowAtPoint[5, 0:288, :] 
+    flowWithAttack[150:200] = flowWithAttack[150:200] + 2*random.uniform(start, end)
+    #flowWithAttack[50:90] = flowWithAttack[50:90] - 2*random.uniform(start, end)
+    flowWithAttack[200:250] = flowWithAttack[200:250] - 2*random.uniform(start, end)
+    
     
     for i in range(0, 288 -len(diff)):
         for j in range(0, len(diff)):
@@ -69,33 +66,18 @@ def Analysis(start):
     for i in range (0, len(attack)):
         for j, g in groupby(enumerate(attack[i]), lambda (k,x):k-x):
             tmpList = map(itemgetter(1), g)
-            var_1 = tmpList[len(tmpList)/2]
-            attack_points[i].append(var_1)
-            var_2 = attack_type_list[i][attack[i].index(var_1)]
-            attack_type[i].append(var_2)
-        
-        print("###############")   
-        print(attack[i])
-        print(attack_type_list[i])
-        print(attack_points[i])
-        print(attack_type[i])
-            #attack_type.append(attack_type_list[i][attack[i].index(var)])
+            var = tmpList[len(tmpList)/2]
+            attack_points[i].append(var)
+        for j, g in groupby(attack_type_list[i]):
+            attack_type[i].append(j)
 
 tmpFlowAtPoint = np.copy(flowAtPoint)
-Analysis (0.20)
+Analysis (0.10)
 flowAtPoint = np.copy(tmpFlowAtPoint)
 print (attack_type)
 print(attack_points)
 
-for i in range(0, len(attack_type)):
-    print("################")
-    print(len(attack_type[i]))
-    print(len(attack_points[i]))
 
-for i in range(0, len(attack)):
-    plt.scatter(attack[i], attack[i])
-    plt.grid()
-    plt.grid()
-    plt.show()
+
          
 
--- dataAnalysis_mean.py
+++ dataAnalysis_mean.py
@@ -36,53 +36,37 @@ attack = [[] for i in range(0, 12)]
 attack_type_list = [[] for i in range(0, 12)]
 attack_points = [[] for i in range(0, 12)]
 attack_type = [[] for i in range(0, 12)]
+variance = []
+standard_deviation = []
 
 for i in range (0, 7):
     for j in range (0, 288):
         mean[i].append(np.mean(flowAtPoint[i:28:7, j, :])) 
+    variance.append(np.var(mean[i]))
+    standard_deviation.append(np.std(mean[i]))
          
-print (np.shape(mean))
+print variance
+print standard_deviation
 
-for i in range (0, len(mean)):
-    plt.plot(mean[i])
-    plt.show()
+threshhold = np.mean(standard_deviation)
 
-for i in range(0, 288 - len(diff)):
-    for j in range(0, len(diff)):
-        diff[j].append( np.absolute(np.mean(flowAtPoint[:, i:i+1, :]) - np.mean(flowAtPoint[:, i+j+1:i+j+1 + j+1, :])) )
-    
-threshhold = [max(i) for i in diff]
 
 def Analysis(start):
 
     end = start + start/100
     flowWithAttack = flowAtPoint[day, 0:288, :] 
-    flowWithAttack[150:200:2] = flowWithAttack[150:200:2] + 2*random.uniform(start, end) 
-    flowWithAttack[151:200:2] = flowWithAttack[151:200:2] + 2*random.uniform(start, end)       
+    flowWithAttack[150:200:2] = flowWithAttack[150:200:2] + random.uniform(start, end) 
+    flowWithAttack[151:200:2] = flowWithAttack[151:200:2] + random.uniform(start, end)       
     
-    for i in range(0, 288 -len(diff)):
-        for j in range(0, len(diff)):
-            if (abs( np.mean(flowWithAttack[i+j+1:i+j+1 + j+1]) - np.mean(flowWithAttack[i:i+j+1])) > threshhold[j]):
-                attack[j].append(i+j+1)
-                attack_type_list[j].append(np.sign(np.mean(flowWithAttack[i+j+1:i+j+1 + j+1]) - np.mean(flowWithAttack[i:i+j+1])))
-    
-    for i in range (0, len(attack)):
-        for j, g in groupby(enumerate(attack[i]), lambda (k,x):k-x):
-            tmpList = map(itemgetter(1), g)
-            var_1 = tmpList[len(tmpList)/2]
-            attack_points[i].append(var_1)
-            var_2 = attack_type_list[i][attack[i].index(var_1)]
-            attack_type[i].append(var_2)
-        
-        print("###############")   
-        print(attack[i])
-        print(attack_type_list[i])
-        print(attack_points[i])
-        print(attack_type[i])
-            #attack_type.append(attack_type_list[i][attack[i].index(var)])
-
+    for i in range(0, 288):
+        for j in range(0, 1):
+            print np.absolute(flowWithAttack[i] -  mean[day][i])
+            if ( np.absolute(flowWithAttack[i] -  mean[day][i]) > threshhold):
+                attack[j].append(i)
+                attack_type_list[j].append(np.sign( flowWithAttack[i] -  mean[day][i] ))
+   
 tmpFlowAtPoint = np.copy(flowAtPoint)
-Analysis (0.20)
+Analysis (0.10)
 flowAtPoint = np.copy(tmpFlowAtPoint)
 print (attack_type)
 print(attack_points)
@@ -92,10 +76,10 @@ for i in range(0, len(attack_type)):
     print(len(attack_type[i]))
     print(len(attack_points[i]))
 
+print (attack)
 for i in range(0, len(attack)):
     plt.scatter(attack[i], attack[i])
     plt.grid()
-    plt.grid()
     plt.show()
-         
+        
 
